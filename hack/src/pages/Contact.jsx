fbe6aa3a3b172925beab2088e9dde583b189f583
kishan kumawat <kishankumawat1271@gamil.com>
2025-09-12 23:30:16 +0530

    add kar diy

---
+++ hack/src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+        <p className='bg-red'>THis is contack page</p>
+        
+      
+    </div>
+  )
+}
+
+export default Contact
