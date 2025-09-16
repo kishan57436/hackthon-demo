ca6b388e76298ad0b5c7eea8b580b59a9faf8222
kishan kumawat <kishankumawat1271@gamil.com>
2025-09-16 22:43:31 +0530

    add the pages

--- hack/src/App.jsx
+++ hack/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Contact from './pages/contact'
+import Rohitpage from './pages/rohitpage'
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,6 +13,7 @@ function App() {
   return (
     <>
     <Contact/>
+    <Rohitpage/>
        
     </>
   )
